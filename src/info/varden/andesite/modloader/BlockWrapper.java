60871d2b21ea48423a26751970d8eb614f8f9111
Marius Lindvall <marius@varden.info>
2015-05-15 22:46:39 +0200

    Indentation consistency

--- src/info/varden/andesite/modloader/BlockWrapper.java
+++ src/info/varden/andesite/modloader/BlockWrapper.java
@@ -6,25 +6,25 @@ import net.minecraft.block.Block;
 import net.minecraftforge.fml.common.registry.GameData;
 
 public class BlockWrapper {
-	
-	private final Block block;
-	
-	private BlockWrapper(Block base) {
-		this.block = base;
-	}
+    
+    private final Block block;
+    
+    private BlockWrapper(Block base) {
+        this.block = base;
+    }
     
     public static BlockWrapper getFor(String name) {
-    	return new BlockWrapper(GameData.getBlockRegistry().getObject(name));
+        return new BlockWrapper(GameData.getBlockRegistry().getObject(name));
     }
     
     public BlockWrapper setLightLevel(float value) {
-    	this.block.setLightLevel(value);
-    	return this;
+        this.block.setLightLevel(value);
+        return this;
     }
     
     public BlockWrapper setStepSound(Block.SoundType sound) {
-    	this.block.setStepSound(sound);
-    	return this;
+        this.block.setStepSound(sound);
+        return this;
     }
     
     public BlockWrapper setResistance(float resistance) {
@@ -33,33 +33,33 @@ public class BlockWrapper {
     }
     
     public BlockWrapper setHardness(float hardness) {
-    	float resistance = -1F;
-    	try {
-	    	Field[] fs = Block.class.getDeclaredFields();
-	    	int floatPassed = 0;
-	    	for (Field f : fs) {
-	    		if (f.getType() == Float.class) {
-	    			if (floatPassed == 1) {
-	    				f.setAccessible(true);
-	    				resistance = (Float) f.get(this.block);
-	    				break;
-	    			}
-	    			floatPassed++;
-	    		}
-	    	}
-    	} catch (Exception ex) {
-    		ex.printStackTrace();
-    	}
-    	this.block.setHardness(hardness);
-    	if (resistance != -1F) {
-    		setResistance(resistance);
-    	}
-    	return this;
+        float resistance = -1F;
+        try {
+            Field[] fs = Block.class.getDeclaredFields();
+            int floatPassed = 0;
+            for (Field f : fs) {
+                if (f.getType() == Float.class) {
+                    if (floatPassed == 1) {
+                        f.setAccessible(true);
+                        resistance = (Float) f.get(this.block);
+                        break;
+                    }
+                    floatPassed++;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        this.block.setHardness(hardness);
+        if (resistance != -1F) {
+            setResistance(resistance);
+        }
+        return this;
     }
     
     public BlockWrapper setSlipperiness(float value) {
-    	this.block.slipperiness = value;
-    	return this;
+        this.block.slipperiness = value;
+        return this;
     }
 
 }
