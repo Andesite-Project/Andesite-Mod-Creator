81bff2eca7dfbf2dadda2c3a804407039d574387
Marius Lindvall <marius@varden.info>
2015-05-16 23:09:18 +0200

    Added some documentation
    More to be done.

--- src/info/varden/andesite/modloader/PlayerWrapper.java
+++ src/info/varden/andesite/modloader/PlayerWrapper.java
@@ -2,22 +2,47 @@ package info.varden.andesite.modloader;
 
 import net.minecraft.entity.player.EntityPlayer;
 
+/**
+ * Wrapper for EntityPlayer.
+ * @author Marius
+ */
 public class PlayerWrapper {
     
+	/**
+	 * The wrapped player.
+	 */
     private final EntityPlayer player;
     
+    /**
+     * Wraps a player entity and creates a PlayerWrapper.
+     * @param player The player to wrap.
+     */
     private PlayerWrapper(EntityPlayer player) {
         this.player = player;
     }
     
+    /**
+     * Creates a PlayerWrapper for the given player.
+     * @param player Player entity to wrap.
+     * @return A PlayerWrapper instance.
+     */
     public static PlayerWrapper getFor(EntityPlayer player) {
         return new PlayerWrapper(player);
     }
     
+    /**
+     * Gets the absorption amount of the player.
+     * @return Absorption amount.
+     */
     public float getAbsorptionAmount() {
         return this.player.getAbsorptionAmount();
     }
     
+    /**
+     * Sets the absorption amount for the player.
+     * @param amount The absorption amount to set.
+     * @return The PlayerWrapper instance.
+     */
     public PlayerWrapper setAbsorptionAmount(float amount) {
         this.player.setAbsorptionAmount(amount);
         return this;
